38b7b39a132365a25542784b6cfb8d6a67cfc7f9
Manthan <manthangigoo10@gmail.com>
2024-07-18 09:07:33 +0000

    created async function in new file

---
+++ server.js
@@ -0,0 +1,5 @@
+const fs = require("node:fs/promises");
+
+require("@sap/cds").on("listening", async () => {
+  await fs.writeFile("listening", "");
+});
--- srv/main.js
+++ srv/main.js
@@ -3,9 +3,13 @@ const cds = require("@sap/cds");
 module.exports = cds.service.impl(function () {
   console.log("I am in a anonymous function");
 
-  this.on("READ", "Books", (_, next) => {
-    console.log("handling read of books");
-
-    next();
+  this.on("READ", "Books", (req, next) => {
+    return [
+      {
+        ID: cds.utils.uuid,
+        title: "The title",
+        author_ID: 2,
+      },
+    ];
   });
 });
