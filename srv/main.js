91116e99c60106fec29affd456e83879bb4c07ce
Manthan <manthangigoo10@gmail.com>
2024-07-20 09:45:20 +0000

    used .after method

--- srv/main.js
+++ srv/main.js
@@ -1,15 +1,19 @@
 const cds = require("@sap/cds");
+const logger = cds.log("capb2b");
 
 module.exports = cds.service.impl(function () {
-  console.log("I am in a anonymous function");
+  logger("I am in a anonymous function");
 
-  this.on("READ", "Books", (req, next) => {
-    return [
-      {
-        ID: cds.utils.uuid,
-        title: "The title",
-        author_ID: 2,
-      },
-    ];
+  // this.on("READ", "Books", (req, next) => {
+  //   return [
+  //     {
+  //       ID: cds.utils.uuid,
+  //       title: "The title",
+  //       author_ID: 2,
+  //     },
+  //   ];
+  // });
+  this.after("READ", "Books", (data, req) => {
+    data.map((book) => (book.title += "!"));
   });
 });
